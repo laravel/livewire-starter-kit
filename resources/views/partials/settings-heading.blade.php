480eed34e4d41192de80092adb31099b11056528
Taylor Otwell <taylor@laravel.com>
2025-02-11 10:39:57 -0600

    wip

--- resources/views/partials/settings-heading.blade.php
+++ resources/views/partials/settings-heading.blade.php
@@ -1,5 +1,5 @@
 <div class="w-full mb-6 relative">
     <flux:heading size="xl" level="1">Settings</flux:heading>
-    <flux:subheading size="lg" class="mb-6">Manage your profile and account setting</flux:subheading>
+    <flux:subheading size="lg" class="mb-6">Manage your profile and account settings</flux:subheading>
     <flux:separator variant="subtle" />
 </div>
