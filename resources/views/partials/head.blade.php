7204c32fb3426b643d876b9ad95b060e5d135fc0
Taylor Otwell <taylor@laravel.com>
2025-02-12 17:34:09 -0600

    fonts

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,5 +3,8 @@
 
 <title>{{ $title ?? 'Laravel' }}</title>
 
+<link rel="preconnect" href="https://fonts.bunny.net" />
+<link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 @fluxStyles
