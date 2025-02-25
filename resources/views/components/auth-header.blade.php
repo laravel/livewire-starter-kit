fd890dc0a443967356247fc4383940f0f85969fa
MartinT <m@rtin.win>
2025-02-25 17:24:58 +0100

    Added font-medium

--- resources/views/components/auth-header.blade.php
+++ resources/views/components/auth-header.blade.php
@@ -4,6 +4,6 @@
 ])
 
 <div class="flex w-full flex-col gap-2 text-center">
-    <flux:heading size="lg">{{ $title }}</flux:heading>
+    <flux:heading size="xl">{{ $title }}</flux:heading>
     <flux:subheading>{{ $description }}</flux:subheading>
 </div>
