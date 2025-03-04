74678fe27b78de3d188ec65abbbf24cda1b7700f
KAMRUZZAMAN <kzamanbn@gmail.com>
2025-03-04 12:02:27 +0600

    added `current` attribute

--- resources/views/components/settings/layout.blade.php
+++ resources/views/components/settings/layout.blade.php
@@ -1,13 +1,13 @@
 <div class="flex items-start max-md:flex-col">
     <div class="mr-10 w-full pb-4 md:w-[220px]">
         <flux:navlist>
-            <flux:navlist.item @class(['bg-zinc-800/[4%] dark:bg-white/[7%] text-zinc-800 dark:text-white' => request()->routeIs('settings.profile')]) :href="route('settings.profile')" wire:navigate>
+            <flux:navlist.item :href="route('settings.profile')" :current="request()->routeIs('settings.profile')" wire:navigate>
                 {{ __('Profile') }}
             </flux:navlist.item>
-            <flux:navlist.item @class(['bg-zinc-800/[4%] dark:bg-white/[7%] text-zinc-800 dark:text-white' => request()->routeIs('settings.password')]) :href="route('settings.password')" wire:navigate>
+            <flux:navlist.item :href="route('settings.password')" :current="request()->routeIs('settings.password')" wire:navigate>
                 {{ __('Password') }}
             </flux:navlist.item>
-            <flux:navlist.item @class(['bg-zinc-800/[4%] dark:bg-white/[7%] text-zinc-800 dark:text-white' => request()->routeIs('settings.appearance')]) :href="route('settings.appearance')" wire:navigate>
+            <flux:navlist.item :href="route('settings.appearance')" :current="request()->routeIs('settings.appearance')" wire:navigate>
                 {{ __('Appearance') }}
             </flux:navlist.item>
         </flux:navlist>
