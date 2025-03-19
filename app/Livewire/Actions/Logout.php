36b06a4b8935348f459bc27057a86aab1283326e
Dominik Mery <dominik@mery.online>
2025-03-19 22:36:30 +0100

    Add combined RedirectResponse|Redirector return type to Logout action

--- app/Livewire/Actions/Logout.php
+++ app/Livewire/Actions/Logout.php
@@ -5,13 +5,14 @@ namespace App\Livewire\Actions;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
+use Livewire\Features\SupportRedirects\Redirector;
 
 class Logout
 {
     /**
      * Log the current user out of the application.
      */
-    public function __invoke(): RedirectResponse
+    public function __invoke(): RedirectResponse|Redirector
     {
         Auth::guard('web')->logout();
 
