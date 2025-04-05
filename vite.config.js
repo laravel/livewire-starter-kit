d43aff01a7b71edac24b07a9b30b2f7c2e380ce4
Van Miranda <van@heyattention.com>
2025-04-05 10:57:56 +0200

    Create guest layout to clarify Guest CSS behavior without inlining Tailwind style

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,11 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/css/guest.css'
+            ],
             refresh: [`resources/views/**/*`],
         }),
         tailwindcss(),
