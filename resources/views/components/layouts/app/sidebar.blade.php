158f555cdb2884a391d4e45153a78f817246866e
hawkiq <osama.sw87@yahoo.com>
2025-09-13 12:35:27 +0300

    Fixing sidebar errors

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -165,16 +165,6 @@
         </flux:dropdown>
     </flux:header>
 
-                    <form method="POST" action="{{ route('logout') }}" class="w-full">
-                        @csrf
-                        <flux:menu.item as="button" type="submit" icon="arrow-right-start-on-rectangle" class="w-full" data-test="logout-button">
-                            {{ __('Log Out') }}
-                        </flux:menu.item>
-                    </form>
-                </flux:menu>
-            </flux:dropdown>
-        </flux:header>
-
     @fluxScripts
 </body>
 
