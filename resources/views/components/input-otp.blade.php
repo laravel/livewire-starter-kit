ead85d1d12f95f2ed993f8ebbf2429eb9cf672cf
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2025-09-22 09:34:41 -0400

    extract x data out to script tag

--- resources/views/components/input-otp.blade.php
+++ resources/views/components/input-otp.blade.php
@@ -7,98 +7,7 @@
     @focus-2fa-auth-code.window="$refs.input1?.focus()"
     @clear-2fa-auth-code.window="clearAll()"
     class="relative"
-    x-data="{
-        totalDigits: @js($digits),
-        digitIndices: @js(range(1, $digits)),
-        init() {
-            $nextTick(() => {
-                this.$refs.input1?.focus();
-            });
-        },
-        getInput(index) {
-            return this.$refs['input' + index];
-        },
-        setValue(index, value) {
-            this.getInput(index).value = value;
-        },
-        getCode() {
-            return this.digitIndices
-                .map(i => this.getInput(i).value)
-                .join('');
-        },
-        updateHiddenField() {
-            this.$refs.code.value = this.getCode();
-            this.$refs.code.dispatchEvent(new Event('input', { bubbles: true }));
-            this.$refs.code.dispatchEvent(new Event('change', { bubbles: true }));
-        },
-        handleNumberKey(index, key) {
-            this.setValue(index, key);
-
-            if (index < this.totalDigits) {
-                this.getInput(index + 1).focus();
-            }
-
-            $nextTick(() => {
-                this.updateHiddenField();
-            });
-        },
-        handleBackspace(index) {
-            const currentInput = this.getInput(index);
-
-            if (currentInput.value !== '') {
-                currentInput.value = '';
-                this.updateHiddenField();
-                return;
-            }
-
-            if (index <= 1) {
-                return;
-            }
-
-            const previousInput = this.getInput(index - 1);
-            previousInput.value = '';
-            previousInput.focus();
-            this.updateHiddenField();
-        },
-        handleKeyDown(index, event) {
-            const key = event.key;
-
-            if (/^[0-9]$/.test(key)) {
-                event.preventDefault();
-                this.handleNumberKey(index, key);
-                return;
-            }
-
-            if (key === 'Backspace') {
-                event.preventDefault();
-                this.handleBackspace(index);
-                return;
-            }
-        },
-        handlePaste(event) {
-            event.preventDefault();
-            const pastedText = (event.clipboardData || window.clipboardData).getData('text');
-            const numericOnly = pastedText.replace(/[^0-9]/g, '');
-            const digitsToFill = Math.min(numericOnly.length, this.totalDigits);
-
-            this.digitIndices
-                .slice(0, digitsToFill)
-                .forEach(index => {
-                    this.setValue(index, numericOnly[index - 1]);
-                });
-
-            if (numericOnly.length >= this.totalDigits) {
-            this.updateHiddenField();
-            }
-        },
-        clearAll() {
-            this.digitIndices.forEach(index => {
-                this.setValue(index, '');
-            });
-            this.$refs.code.value = '';
-            this.$refs.input1?.focus();
-        }
-    }"
+    x-data="inputOtp()"
 >
     <div class="flex items-center">
         @for ($x = 1; $x <= $digits; $x++)
@@ -132,3 +41,100 @@
         maxlength="{{ $digits }}"
     />
 </div>
+
+<script>
+     document.addEventListener('alpine:init', () => {
+        Alpine.data('inputOtp', () => ({
+            totalDigits: @js($digits),
+            digitIndices: @js(range(1, $digits)),
+            init() {
+                this.$nextTick(() => {
+                    this.$refs.input1?.focus();
+                });
+            },
+            getInput(index) {
+                return this.$refs['input' + index];
+            },
+            setValue(index, value) {
+                this.getInput(index).value = value;
+            },
+            getCode() {
+                return this.digitIndices
+                    .map(i => this.getInput(i).value)
+                    .join('');
+            },
+            updateHiddenField() {
+                this.$refs.code.value = this.getCode();
+                this.$refs.code.dispatchEvent(new Event('input', { bubbles: true }));
+                this.$refs.code.dispatchEvent(new Event('change', { bubbles: true }));
+            },
+            handleNumberKey(index, key) {
+                this.setValue(index, key);
+
+                if (index < this.totalDigits) {
+                    this.getInput(index + 1).focus();
+                }
+
+                this.$nextTick(() => {
+                    this.updateHiddenField();
+                });
+            },
+            handleBackspace(index) {
+                const currentInput = this.getInput(index);
+
+                if (currentInput.value !== '') {
+                    currentInput.value = '';
+                    this.updateHiddenField();
+                    return;
+                }
+
+                if (index <= 1) {
+                    return;
+                }
+
+                const previousInput = this.getInput(index - 1);
+                previousInput.value = '';
+                previousInput.focus();
+                this.updateHiddenField();
+            },
+            handleKeyDown(index, event) {
+                const key = event.key;
+
+                if (/^[0-9]$/.test(key)) {
+                    event.preventDefault();
+                    this.handleNumberKey(index, key);
+                    return;
+                }
+
+                if (key === 'Backspace') {
+                    event.preventDefault();
+                    this.handleBackspace(index);
+                    return;
+                }
+            },
+            handlePaste(event) {
+                event.preventDefault();
+                const pastedText = (event.clipboardData || window.clipboardData).getData('text');
+                const numericOnly = pastedText.replace(/[^0-9]/g, '');
+                const digitsToFill = Math.min(numericOnly.length, this.totalDigits);
+
+                this.digitIndices
+                    .slice(0, digitsToFill)
+                    .forEach(index => {
+                        this.setValue(index, numericOnly[index - 1]);
+                    });
+
+                if (numericOnly.length >= this.totalDigits) {
+                    this.updateHiddenField();
+                }
+            },
+            clearAll() {
+                this.digitIndices.forEach(index => {
+                    this.setValue(index, '');
+                });
+                this.$refs.code.value = '';
+                this.$refs.input1?.focus();
+            }
+        }))
+    })
+</script>
