a7556eceb3ff3e6eeb16f4fa1bc7815add415d80
Josadec Pedraza <josadec@gmail.com>
2025-12-11 17:19:29 -0800

    New update from Mau

--- app/Http/Controllers/PartController.php
+++ app/Http/Controllers/PartController.php
@@ -6,5 +6,23 @@ use Illuminate\Http\Request;
 
 class PartController extends Controller
 {
-    //
+    public function index()
+    {
+       //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function show(Request $request)
+    {
+        //
+    }
+
+    public function edit(Request $request)
+    {
+        //
+    }
 }
