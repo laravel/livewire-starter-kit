74699d65a436a86fab5bed840b51f53fd1e15fd0
Wendell Adriel <wendelladriel.ti@gmail.com>
2026-01-07 14:51:29 +0000

    Update factory and seeder to not enable 2FA by default

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -20,7 +20,7 @@ class AuthenticationTest extends TestCase
 
     public function test_users_can_authenticate_using_the_login_screen(): void
     {
-        $user = User::factory()->withoutTwoFactor()->create();
+        $user = User::factory()->create();
 
         $response = $this->post(route('login.store'), [
             'email' => $user->email,
@@ -58,7 +58,7 @@ class AuthenticationTest extends TestCase
             'confirmPassword' => true,
         ]);
 
-        $user = User::factory()->create();
+        $user = User::factory()->withTwoFactor()->create();
 
         $response = $this->post(route('login.store'), [
             'email' => $user->email,
