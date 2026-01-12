e980b2188af24fdccb1c1478621ca998a391c466
Wendell Adriel <wendelladriel.ti@gmail.com>
2026-01-12 20:07:49 +0000

    Remove not wanted changes in blade template

--- resources/views/livewire/settings/delete-user-form.blade.php
+++ resources/views/livewire/settings/delete-user-form.blade.php
@@ -32,8 +32,7 @@ new class extends Component {
     </div>
 
     <flux:modal.trigger name="confirm-user-deletion">
-        <flux:button variant="danger" x-data="" x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion')"
-                     data-test="delete-user-button">
+        <flux:button variant="danger" x-data="" x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion')" data-test="delete-user-button">
             {{ __('Delete account') }}
         </flux:button>
     </flux:modal.trigger>
@@ -48,7 +47,7 @@ new class extends Component {
                 </flux:subheading>
             </div>
 
-            <flux:input wire:model="password" :label="__('Password')" type="password"/>
+            <flux:input wire:model="password" :label="__('Password')" type="password" />
 
             <div class="flex justify-end space-x-2 rtl:space-x-reverse">
                 <flux:modal.close>
--- resources/views/livewire/settings/password.blade.php
+++ resources/views/livewire/settings/password.blade.php
@@ -44,8 +44,7 @@ new class extends Component {
 
     <flux:heading class="sr-only">{{ __('Password Settings') }}</flux:heading>
 
-    <x-settings.layout :heading="__('Update password')"
-                       :subheading="__('Ensure your account is using a long, random password to stay secure')">
+    <x-settings.layout :heading="__('Update password')" :subheading="__('Ensure your account is using a long, random password to stay secure')">
         <form method="POST" wire:submit="updatePassword" class="mt-6 space-y-6">
             <flux:input
                 wire:model="current_password"
