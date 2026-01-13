91bb04332daa28194f64f3dc7fdee107761cb09d
Caleb Porzio <calebporzio@gmail.com>
2026-01-13 09:25:41 -0500

    wip

--- resources/views/pages/settings/⚡password.blade.php
+++ resources/views/pages/settings/⚡password.blade.php
@@ -41,7 +41,7 @@ new class extends Component {
 
     <flux:heading class="sr-only">{{ __('Password Settings') }}</flux:heading>
 
-    <x-settings.layout :heading="__('Update password')" :subheading="__('Ensure your account is using a long, random password to stay secure')">
+    <x-pages::settings.layout :heading="__('Update password')" :subheading="__('Ensure your account is using a long, random password to stay secure')">
         <form method="POST" wire:submit="updatePassword" class="mt-6 space-y-6">
             <flux:input
                 wire:model="current_password"
@@ -77,5 +77,5 @@ new class extends Component {
                 </x-action-message>
             </div>
         </form>
-    </x-settings.layout>
+    </x-pages::settings.layout>
 </section>
--- resources/views/pages/settings/⚡profile.blade.php
+++ resources/views/pages/settings/⚡profile.blade.php
@@ -74,7 +74,7 @@ new class extends Component {
 
     <flux:heading class="sr-only">{{ __('Profile Settings') }}</flux:heading>
 
-    <x-settings.layout :heading="__('Profile')" :subheading="__('Update your name and email address')">
+    <x-pages::settings.layout :heading="__('Profile')" :subheading="__('Update your name and email address')">
         <form wire:submit="updateProfileInformation" class="my-6 w-full space-y-6">
             <flux:input wire:model="name" :label="__('Name')" type="text" required autofocus autocomplete="name" />
 
@@ -113,6 +113,6 @@ new class extends Component {
             </div>
         </form>
 
-        <livewire:settings.delete-user-form />
-    </x-settings.layout>
+        <livewire:pages::settings.delete-user-form />
+    </x-pages::settings.layout>
 </section>
