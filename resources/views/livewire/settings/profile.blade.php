b7fe004370856d10f439a011a9b61a64a41cb671
Wendell Adriel <wendelladriel.ti@gmail.com>
2026-01-14 11:40:58 +0000

    Revert not needed changes

--- resources/views/livewire/settings/profile.blade.php
+++ resources/views/livewire/settings/profile.blade.php
@@ -3,7 +3,7 @@
 
     <flux:heading class="sr-only">{{ __('Profile Settings') }}</flux:heading>
 
-    <x-pages::settings.layout :heading="__('Profile')" :subheading="__('Update your name and email address')">
+    <x-settings.layout :heading="__('Profile')" :subheading="__('Update your name and email address')">
         <form wire:submit="updateProfileInformation" class="my-6 w-full space-y-6">
             <flux:input wire:model="name" :label="__('Name')" type="text" required autofocus autocomplete="name" />
 
@@ -40,6 +40,6 @@
             </div>
         </form>
 
-        <livewire:pages::settings.delete-user-form />
-    </x-pages::settings.layout>
+        <livewire.settings.delete-user-form />
+    </x-settings.layout>
 </section>
