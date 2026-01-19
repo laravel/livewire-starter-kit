0d5b363ac5fb7c71dbc80e89d70a4773da57d527
Wendell Adriel <wendelladriel.ti@gmail.com>
2026-01-19 16:28:47 +0000

    Add script to rename files on after install hook

---
+++ livewire_filenames.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * This script adds the Livewire ⚡ emoji prefix to blade files.
+ * Run after composer create-project to add emojis to filenames.
+ */
+
+$basePath = __DIR__.'/resources/views/pages/settings';
+
+$files = [
+    'appearance.blade.php',
+    'delete-user-form.blade.php',
+    'password.blade.php',
+    'profile.blade.php',
+    'two-factor.blade.php',
+    'two-factor/recovery-codes.blade.php',
+];
+
+foreach ($files as $file) {
+    $currentPath = $basePath.'/'.$file;
+    $directory = dirname($currentPath);
+    $filename = basename($file);
+    $newPath = $directory.'/⚡'.$filename;
+
+    if (file_exists($currentPath) && ! file_exists($newPath)) {
+        rename($currentPath, $newPath);
+        echo "Renamed: {$file} -> ⚡{$filename}\n";
+    }
+}
